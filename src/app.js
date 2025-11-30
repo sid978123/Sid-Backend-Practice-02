f6ea7527a1a1b22ae8794bade8455a218eff23ef
sid978123 <siddharthaman978@gmail.com>
2025-11-30 23:09:47 +0530

    fix: correct usage of cors middleware in app.js

--- src/app.js
+++ src/app.js
@@ -4,14 +4,16 @@ import cors from "cors";
 
 const app = express();
 
-app.cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-});
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.cookieParser());
+app.use(cookieParser());
 app.use(express.static("public"));
 
 export { app };
