c1a80a8c7e628365fda5f111982b50f7770fa4ef
sid978123 <siddharthaman978@gmail.com>
2025-11-30 22:47:59 +0530

    feat: add ApiError, ApiResponse, and asyncHandler classes for error handling and response management

---
+++ src/ApiResponse.js
@@ -0,0 +1,7 @@
+class ApiResponse {
+  constructor(statusCode, data, message = "success") {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
