c2d79eea9ed2142f6f93f246a59fd033350c26e0
sid978123 <siddharthaman978@gmail.com>
2025-11-30 23:05:14 +0530

    feat: configure CORS and add middleware for JSON and URL-encoded request handling

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+});
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.cookieParser());
+app.use(express.static("public"));
+
 export { app };
